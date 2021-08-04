de7d7e30fa92aa6105edc35f0e47c0a5e24181f2
Somyaranjan Rout <45004977+somyaranjan26@users.noreply.github.com>
2021-08-04 08:58:51 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import 'firebase/storage';
 import 'firebase/firestore';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCDY2CkmkBX0fFlwmBgy-7R8w45TcGZLQo",
-    authDomain: "morningstar-b0754.firebaseapp.com",
-    projectId: "morningstar-b0754",
-    storageBucket: "morningstar-b0754.appspot.com",
-    messagingSenderId: "553887133322",
-    appId: "1:553887133322:web:aa162aeea53be1527a8773"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
